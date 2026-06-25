19658cda919138779399e9f4128e81df4f9d9ed2
Favour Kalu <dkalu2004@gmail.com>
2026-06-25 11:24:50 +0100

    removed error boundary from components second trial

--- app/error.jsx
+++ app/error.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ErrorBoundary from "./components/ErrorBoundary";
+// import ErrorBoundary from "./components/ErrorBoundary";
 
 export default function Error({ error, reset }) {
   return <ErrorBoundary error={error} reset={reset} />;
